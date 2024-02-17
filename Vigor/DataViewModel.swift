b898a114b87fdf4377aa3853b2359283c3e280f2
Vastly <caseytraina@gmail.com>
2024-02-17 01:30:45 -0800

    Added Data View Model

---
+++ Vigor/DataViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  DataViewModel.swift
+//  Vigor
+//
+//  Created by Casey Traina on 2/17/24.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+
+@MainActor
+
+class DataViewModel: ObservableObject {
+    
+    init() {
+        
+    }
+    
+    private func getDocument(_ path: DocumentReference) async -> [String : Any]? {
+        do {
+            let snapshot = try await path.getDocument()
+            let data = snapshot.data()
+            return data
+        } catch {
+            print("There was an error querying: \(error)")
+        }
+        return nil
+    }
+    
+}
