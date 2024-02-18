e2f7fb1bc5f43e332dfaf6114bb5a2e04e64b761
FilipHenriksson <103415153+FilipHenriksson@users.noreply.github.com>
2024-02-17 22:13:32 -0800

    Add files via upload

---
+++ Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Vigor
+//
+//  Created by Casey Traina on 2/16/24.
+//
+
+import Foundation
+//import SwiftData
+
+//@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
